3dee370931d93f968cc8abcada5c90fbb340901c
m1y4z0n0 <m1y4z0n0@gmail.com>
2026-09-10 22:51:54 +0800

    修编译错误：onResume 需用 public 覆盖（BridgeActivity 中为 public）

--- android/app/src/main/java/com/mealmeter/app/MainActivity.java
+++ android/app/src/main/java/com/mealmeter/app/MainActivity.java
@@ -43,7 +43,7 @@ public class MainActivity extends BridgeActivity {
     }
 
     @Override
-    protected void onResume() {
+    public void onResume() {
         super.onResume();
         pushInsets();
     }
